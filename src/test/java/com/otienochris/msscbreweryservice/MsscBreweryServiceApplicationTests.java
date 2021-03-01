82ca385476d5e63877056a63b6bc8fb4684e493a
otienochris <otienochris98@gmail.com>
2021-03-01 22:01:18 +0300

    Initial project commit

---
+++ src/test/java/com/otienochris/msscbreweryservice/MsscBreweryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.otienochris.msscbreweryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBreweryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
