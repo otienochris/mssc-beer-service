0320187427577f3ae440e563a23d27094e1ebe84
otienochris <otienochris98@gmail.com>
2021-03-09 17:20:45 +0300

    adding a bootstrap class

---
+++ src/main/java/com/otienochris/msscbreweryservice/bootstrap/BeerLoader.java
@@ -0,0 +1,44 @@
+package com.otienochris.msscbreweryservice.bootstrap;
+
+import com.otienochris.msscbreweryservice.domain.Beer;
+import com.otienochris.msscbreweryservice.repositories.BeerRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+
+@Component
+public class BeerLoader implements CommandLineRunner {
+
+    private final BeerRepository beerRepository;
+
+    public BeerLoader(BeerRepository beerRepository) {
+        this.beerRepository = beerRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        loadBeerObjects();
+    }
+
+    private void loadBeerObjects() {
+        if (beerRepository.count() == 0 ){
+            beerRepository.save(Beer.builder()
+                    .beerName("Mango Bobs")
+                    .beerStyle("IPA")
+                    .quantityToBrew(200)
+                    .minOnHand(12)
+                    .upc(14545346345L)
+                    .price(new BigDecimal("12.59"))
+                    .build());
+            beerRepository.save(Beer.builder()
+                    .beerName("Galaxy Cat")
+                    .beerStyle("PALE_ALE")
+                    .quantityToBrew(200)
+                    .minOnHand(13)
+                    .upc(145453463695L)
+                    .price(new BigDecimal("11.87"))
+                    .build());
+        }
+    }
+}
